31022d7325e6bc87ffa7d4fbb66d8bacb02503de
Frederick Roegiers <frederick@rogerthat.be>
2023-03-13 11:44:03 +0100

    wip

--- src/docs/paths/users.js
+++ src/docs/paths/users.js
@@ -1,10 +1,27 @@
 export default {
-  "/users": {
+  "/api/users": {
     summary: "CRUD with users",
     description: "Get all users in the database",
     get: {
       tags: ["Users"],
       summary: "Get all users",
+    },
+    post: {
+      tags: ["Users"],
+      summary: "Create a new user",
+      requestBody: {
+        content: {
+          required: true,
+          "application/json": {
+            schema: {
+              $ref: "#/components/schemas/User",
+            },
+          },
+        },
+      },
+    },
+    put: {
+      tags: ["Users"],
     },
   },
 };
