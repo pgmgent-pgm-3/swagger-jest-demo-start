fdb4c076996b98ab905a404ab819e082433b1fcc
Frederick Roegiers <frederick@rogerthat.be>
2023-03-14 09:30:25 +0100

    wip

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,6 @@ import Interest from "./Interest.js";
 import NavigationItem from "./NavigationItem.js";
 import Role from "./Role.js";
 import User from "./User.js";
-// this is the new import
 import UserMeta from "./UserMeta.js";
 
 export default [User, NavigationItem, Interest, UserMeta, Role];
